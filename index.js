5190d808d00ad0f9f61df0438da82d07a1e89ef8
Chris Chicas <christianchicas7@gmail.com>
2021-10-21 20:12:24 -0400

    Fixed html and coded w/m/a expenses

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+for(i = 0; i < weeklyExpenseQuestions.length; i++) {
+    let answer = window.prompt(weeklyExpenseQuestions[i]);
+    weeklyExpenses = weeklyExpenses + parseInt(answer);
+}
+
+for(i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let answer = window.prompt(monthlyExpenseQuestions[i]);
+    monthlyExpenses = monthlyExpenses + parseInt(answer);
+}
+
+for(i = 0; i < annualExpenseQuestions.length; i++) {
+    let answer = window.prompt(annualExpenseQuestions[i]);
+    annualExpenses = annualExpenses + parseInt(answer);
+}
